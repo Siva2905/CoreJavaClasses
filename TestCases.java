38c8e91e17222ad7dc3ab06b5094260976d01d44
Pesarlanka Siva Nageswara Rao <113156763+Siva2905@users.noreply.github.com>
2022-09-14 10:16:51 +0530

    simpleInheritance

--- TestCases.java
+++ TestCases.java
@@ -1,45 +1,58 @@
-package inheritanceByObject;
+package simpleInheritance;
 
 import org.junit.Test;
 
-public class TestCases // extends TestReusableComponents
+//Simple Inheritance--- Directly child class(subclass) will inheritate the parent class(super class)
 
-// Signature of the Object using Constructor
-
-// Modifier   ClassName   objName = new Constructor(); // Here ClassName & Constructor Name are same but the diff is paranthess is given to constructor where as paranthess ins not given to ClassName 
-// Constructor is a Method -for Methods we call them with paranthess
-
-{
-   // public TestReusableComponents trc = new TestReusableComponents();
-//    public TestReusableComponents trc1 = new TestReusableComponents("Internet Explo");
-    public TestReusableComponents trc2 = new TestReusableComponents("Internet Explo",95);
-
-// From above constructors we can call any one based on the customer request .
-    
+// Child class(Subclass)
+public class TestCases extends TestResuableComponents
+{	
 	@Test
-	public void composeAndSendmail()
-	{
-		System.out.println("Compose And Send Mail");
-		trc2.launchApplication();
-		trc2.logintoApplication();
-		trc2.compose();
-		trc2.send();
-		trc2.logout();
-		trc2.close();
-		
-	}
+     public void composeAndSendMail()
+     {
+    	 System.out.println("Test Case Compose And Send Mail");
+    	 launchApplication();
+    	 logintoApplication();
+    	 compose();
+    	 send();
+    	 logout();
+    	 close();
+     }
 	
 	@Test
-	public void replymail()
-	{
-		System.out.println("Reply to The Mail");
-		trc2.launchApplication();
-		trc2.logintoApplication();
-		trc2.open();
-		trc2.reply();
-		trc2.logout();
-		trc2.close();
-		
-	}
-
+     public void replyToMail()
+     {
+    	 System.out.println("Tast Case Reply To Mail");
+    	 launchApplication();
+    	 logintoApplication();
+    	 open();
+    	 reply();
+    	 logout();
+    	 close();
+     }
+	
+	@Test
+     public void ForwardMail()
+     {
+    	 System.out.println("Tast Case Forward Mail");
+    	 launchApplication();
+    	 logintoApplication();
+    	 open();
+    	 forward();
+    	 logout();
+    	 close();
+     }
+	
+	@Test
+     public void DeleteMail()
+     {
+    	 System.out.println("Tast Case Forward Mail");
+    	 launchApplication();
+    	 logintoApplication();
+    	 open();
+    	 delete();
+    	 logout();
+    	 close();
+     }
+     
 }
---
+++ TestResuableComponents.java
@@ -0,0 +1,48 @@
+package simpleInheritance;
+
+//Simple Inheritance--- Directly child class(subclass) will inheritate the parent class(super class)
+//                         Parent Class (Super class)
+public class TestResuableComponents 
+{
+	public void launchApplication()
+	{
+		System.out.println("RC : Launch application");
+	}
+	public void logintoApplication()
+	{
+		System.out.println("RC : Log into Application");
+	}
+	public void compose()
+	{
+		System.out.println("RC : Compose the Mail");
+	}
+	public void send()
+	{
+		System.out.println("RC : Send the Mail");
+	}
+	public void logout()
+	{
+		System.out.println("RC : Log Out From Mail");
+	}
+	public void close()
+	{
+		System.out.println("RC : Close the Mail");
+	}
+	public void open()
+	{
+		System.out.println("RC : Open the Mail");
+	}
+	public void reply()
+	{
+		System.out.println("RC : Reply the Mail");
+	}
+	public void forward()
+	{
+		System.out.println("RC : Forward the Mail");
+	}
+	public void delete()
+	{
+		System.out.println("RC : Delete the Mail");
+	}
+	
+}
